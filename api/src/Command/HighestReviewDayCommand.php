598136dca5fb9e15cf93709824770fef672dc4ee
Kolade Ige <kolade.ige@gmail.com>
2024-06-20 19:59:01 +0100

    Console Command: Task 3.2 completed

--- api/src/Command/HighestReviewDayCommand.php
+++ api/src/Command/HighestReviewDayCommand.php
@@ -31,25 +31,45 @@ class HighestReviewDayCommand extends Command
     {
         $this
             ->setDescription('Displays the day with the highest number of published reviews');
+
+        $this
+            ->addOption(
+                name: 'month',
+                mode: InputOption::VALUE_NONE,
+                description: 'If set, the task will display the month instead of the day'
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
+        $isMonth = $input->getOption('month');
         $qb =$this->entityManager->createQueryBuilder();
-        $qb->select('DATE_FORMAT(r.publishedAt, \'YYYY-MM-DD\') as date, COUNT(r.id) as review_count')
-            ->from('App\Entity\Review', 'r')
-            ->groupBy('date')
-            ->orderBy('review_count', 'desc')
-            ->addOrderBy('date', 'desc');
+        $query = null;
+        $period = $isMonth?"month":"day";
+        if($isMonth){
+            $qb->select('DATE_FORMAT(r.publishedAt, \'YYYY-MM\') as date, COUNT(r.id) as review_count')
+                ->from('App\Entity\Review', 'r')
+                ->groupBy('date')
+                ->orderBy('review_count', 'desc')
+                ->addOrderBy('date', 'desc');
 
-        $query = $qb->getQuery();
+            $query = $qb->getQuery();
+        } else {
+            $qb->select('DATE_FORMAT(r.publishedAt, \'YYYY-MM-DD\') as date, COUNT(r.id) as review_count')
+                ->from('App\Entity\Review', 'r')
+                ->groupBy('date')
+                ->orderBy('review_count', 'desc')
+                ->addOrderBy('date', 'desc');
+
+            $query = $qb->getQuery();
+        }
+
         $query->setMaxResults(1);
-
         $result = $query->getSingleResult();
 
         if ($result) {
-            $io->success('The day with the highest number of published reviews is: ' . $result['date'] . ' with count of: ' . $result['review_count']);
+            $io->success("The $period with the highest number of published reviews is: " . $result['date'] . ' with count of: ' . $result['review_count']);
         } else
         {
             $io->warning('No reviews found.');
